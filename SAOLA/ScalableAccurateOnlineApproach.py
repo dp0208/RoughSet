faff55cac691f213ebd9fcc760819eae38e3ea06
MusicBear <zengwuxu@gmail.com>
2019-09-05 14:54:09 +0800

    Revert "Revert "Add SAOLA algorithm""
    This reverts commit e698ab36e2eebcb9cf2d99f1b5c25995ed26f924.

---
+++ SAOLA/ScalableAccurateOnlineApproach.py
@@ -0,0 +1,51 @@
+"""
+Towards Scalable and Accurate Online Feature Selection for Big Data
+author: Kui Yu1, Xindong Wu, Wei Ding, and Jian Pei
+https://doi.org/10.1109/ICDM.2014.63
+"""
+
+
+from mutual_information import *
+
+
+class OnlineFeatureSelectionAdapted3Max:
+    def __init__(self, universe, conditional_features, decision_features, delta_1):
+        self.universe = universe
+        self.conditional_features = conditional_features
+        self.decision_features = decision_features
+        self.delta_1 = delta_1
+        self.delta_2 = 0
+        return
+
+    def get_new_feature(self):
+        """
+        to transfer a feature to the algorithm through yield
+        :return: None
+        """
+        conditional_features = self.conditional_features
+        for i in range(len(conditional_features)):
+            yield conditional_features[i]
+        return None
+
+    # unchecked
+    def run(self):
+        candidate_features = []
+        candidate_features_mi = []
+        for feature in self.get_new_feature():
+            feature_mi = mutual_information(feature, self.decision_features)
+            if feature_mi < self.delta_1:
+                continue
+            flag = True
+            for i in range(len(candidate_features)):
+                if (candidate_features_mi[i] > feature_mi) and (candidate_features_mi[i] >= self.delta_2):
+                    flag = False
+                    break
+                if (feature_mi > candidate_features_mi[i]) and (feature_mi >= self.delta_2):
+                    candidate_features.pop(i)
+                    candidate_features_mi.pop(i)
+                    i -= 1
+                    break
+            if flag:
+                candidate_features.append(feature)
+                candidate_features_mi.append(feature_mi)
+        return candidate_features
